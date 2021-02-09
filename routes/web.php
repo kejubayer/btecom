b5ee0947be075787eedaf8ad42c4aa7658bb0084
kejubayer <khondokereftakharjubayer@gmail.com>
2021-02-09 21:57:37 +0600

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,12 @@ Route::get('/', [App\Http\Controllers\Frontend\HomeController::class,'index'])->
 Route::get('/contact', [App\Http\Controllers\Frontend\HomeController::class,'contact'])->name('contact');
 Route::get('/products/show', [App\Http\Controllers\Frontend\ProductController::class,'show'])->name('product.show');
 
+//admin Dashboard
+Route::get('/admin',[\App\Http\Controllers\Backend\DashboardController::class,'index'])->name('admin.dashboard');
+
+//admin users
+Route::get('/admin/users',[\App\Http\Controllers\Backend\UserController::class,'index'])->name('admin.user');
+
+//admin login
+Route::get('/admin/login',[\App\Http\Controllers\Backend\LoginController::class,'index'])->name('admin.login');
+Route::post('/admin/login',[\App\Http\Controllers\Backend\LoginController::class,'submitLogin']);
